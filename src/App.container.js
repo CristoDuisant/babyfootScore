54da2062382a07adaacb7da4414feed60a328e75
Antoine Regembal <33549142+Mecadie@users.noreply.github.com>
2019-08-09 16:53:26 +0200

    Update App.container.js

--- src/App.container.js
+++ src/App.container.js
@@ -5,8 +5,8 @@ import App from "./App";
 const EnhancedApp = () => {
   const [saison, updateSaison] = useState({ Antoine: 3, Cristofer: 1 });
   const [currentMatch, updateCurrentMatch] = useState({
-    Antoine: 0,
-    Cristofer: 0
+    Antoine: 1,
+    Cristofer: 2
   });
   const [players, updatePlayers] = useState(["Antoine", "Cristofer"]);
 
