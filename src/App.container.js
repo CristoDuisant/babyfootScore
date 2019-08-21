34614663006685e8cd5b3eeb8b8ae76245110bb2
CristoDuisant <cdcris62@gmail.com>
2019-08-21 15:56:57 +0200

    Update App.container.js

--- src/App.container.js
+++ src/App.container.js
@@ -6,7 +6,7 @@ const EnhancedApp = () => {
   const [saison, updateSaison] = useState({ Antoine: 3, Cristofer: 1 });
   const [currentMatch, updateCurrentMatch] = useState({
     Antoine: 1,
-    Cristofer: 3
+    Cristofer: 4
   });
   const [players, updatePlayers] = useState(["Antoine", "Cristofer"]);
 
