3ae338e7b7a834f68be0fccef13f2e1f3aba1c38
REGEMBAL Antoine <S643414@siege.axa-fr.intraxa>
2019-10-03 14:10:44 +0200

    new score.

--- src/App.container.js
+++ src/App.container.js
@@ -5,7 +5,7 @@ import App from "./App";
 const EnhancedApp = () => {
   const [saison, updateSaison] = useState({ Antoine: 0, Cristofer: 0 });
   const [currentMatch, updateCurrentMatch] = useState({
-    Antoine: 2,
+    Antoine: 3,
     Cristofer: 3
   });
   const [players, updatePlayers] = useState(["Antoine", "Cristofer"]);
